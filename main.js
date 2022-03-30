91582e101a0046c6d30779f1376f0f29511eefe6
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-30 15:58:19 +0200

    stesura esercizio

---
+++ main.js
@@ -0,0 +1,8 @@
+new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'Hello Vue!',
+            img: 'https://randomwordgenerator.com/img/picture-generator/53e1d7464a50ad14f1dc8460962e33791c3ad6e04e507749742c78d6904bc0_640.jpg'
+    }
+})
